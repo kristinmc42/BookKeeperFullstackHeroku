7c8cd0416af18131d3f96041c0d7378f17975c8b
kristinmc42 <mccollum.k.j@gmail.com>
2023-01-01 12:18:20 -0500

    added bookInfo component to display single book

--- client/src/pages/SearchBooks.tsx
+++ client/src/pages/SearchBooks.tsx
@@ -4,11 +4,10 @@ import axios from "axios";
 
 // components
 import SearchBar from "../components/SearchBar";
-
+import BookInfo from "../components/BookInfo";
 
 const SearchBooks: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>();
-
 
   const getBooks = async () => {
     return axios
@@ -58,35 +57,7 @@ const SearchBooks: React.FC = () => {
       />
       <ul>
         {data &&
-          data.map((item: any) => {
-             return(
-              <li key={item.id}>
-                {item.volumeInfo.imageLinks.smallThumbnail ? (
-                  <img
-                    src={item.volumeInfo.imageLinks.smallThumbnail}
-                    alt={`Cover of {item.volumeInfo.title}`}
-                  />
-                ) : null}
-                <h2>{item.volumeInfo.title}</h2>
-                {item.volumeInfo.authors
-                  ? item.volumeInfo.authors.map((author: string, index:number) => (
-                      <h3 key={index+item.id}>
-                        <span>By: </span>
-                        {author}
-                      </h3>
-                    ))
-                  : null}
-                {item.volumeInfo.categories
-                  ? item.volumeInfo.categories.map((genre: string, index:number) => (
-                      <h3 key={index+item.id}>
-                        <span>Genre: </span>
-                        {genre}
-                      </h3>
-                    ))
-                  : null}
-              </li>
-            );
-          })}
+            data.map((item: any, index: number) =>  <BookInfo item={item} key={index} />)}
       </ul>
     </div>
   );

0f1ebe44daf1bb03df910a3ab8986c2d024a5a27
kristinmc42 <mccollum.k.j@gmail.com>
2023-01-23 01:01:37 -0500

    changed db variables for deploy

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,10 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: process.env.MY_SQL_DB_HOST || process.env.MYSQLHOST,
-  user: process.env.MY_SQL_DB_USER || process.env.MYSQLUSER,
-  password: process.env.MY_SQL_DB_PASSWORD || process.env.MYSQLPASSWORD,
-  database: process.env.MY_SQL_DB_DATABASE || process.env.MYSQLDATABASE,
+  host: process.env.MY_SQL_DB_HOST,
+  user: process.env.MY_SQL_DB_USER,
+  password: process.env.MY_SQL_DB_PASSWORD,
+  database: process.env.MY_SQL_DB_DATABASE,
 });
 
 db.connect(function (error) {

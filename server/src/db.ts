b0a9a21d8ca08706d9d571795f1ff33b42d0f9cd
kristinmc42 <mccollum.k.j@gmail.com>
2023-01-25 21:31:28 -0500

    hardocding db localhost

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: process.env.MY_SQL_DB_HOST,
+  host: "localhost",
   user: process.env.MY_SQL_DB_USER,
   password: process.env.MY_SQL_DB_PASSWORD,
   database: process.env.MY_SQL_DB_DATABASE,
+  port: 7358,
 });
 
 db.connect(function (error) {

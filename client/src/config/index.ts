19cbb6d06159a1e921d0525abf84f9fc08541e3b
kristinmc42 <mccollum.k.j@gmail.com>
2023-02-17 10:52:43 -0500

    not credentials

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const Axios = axios.create({
     baseURL: `https://${process.env.REACT_APP_API_URL}`,
-    withCredentials: true,
+    // withCredentials: true,
 })

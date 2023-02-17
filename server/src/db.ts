604c843970a890c218bdc3c1b97f37b232ed280e
kristinmc42 <mccollum.k.j@gmail.com>
2023-02-16 22:46:54 -0500

    deleted commented out code

--- server/src/db.ts
+++ server/src/db.ts
@@ -127,16 +127,3 @@ export const updateDbBook = async (
   return data;
 };
 
-// import mysql from "mysql"
-
-// export const db = mysql.createConnection(process.env.DATABASE_URL || "{}")
-
-// console.log("connected to planetscale")
-
-// db.connect(function (error) {
-//   if (error) {
-//     throw error;
-//   } else {
-//     console.log("MySQL db is connected successfully!");
-//   }
-// });

899d88fcfb5300f594bb247c553b8c2055ee45e3
kristinmc42 <mccollum.k.j@gmail.com>
2023-02-23 14:30:34 -0500

    changed base url in config index.ts

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 let url: string = `http://localhost:5000`;
 if (process.env.NODE_ENV === "production") {
-  url = `https://${process.env.REACT_APP_API_URL}`;
+  url = `${process.env.REACT_APP_API_URL}`;
 }
 
 export const Axios = axios.create({

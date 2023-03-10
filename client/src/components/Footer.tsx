b6498405a45a52710a34de48c64c01a800ad305e
kristinmc42 <mccollum.k.j@gmail.com>
2023-03-10 10:27:35 -0500

    added link to github in footer; updated Readme

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,6 +5,8 @@ const Footer: React.FC = () => {
   return (
     <StyledFooter>
       <p>Book Keeper</p>
+      <a href="https://github.com/kristinmc42/BookKeeperFullstackHeroku" target="_blank"
+                rel="noreferrer">How I built this</a>
       <p>Designed by Kristin McCollum</p>
     </StyledFooter>
   );
@@ -19,14 +21,18 @@ const StyledFooter = styled.footer`
   color: ${(props) => props.theme.colors.whiteText};
   padding-top: 1em;
 
-  p {
+  p, a {
     font-size: 0.8rem;
     padding: 10px 30px;
+    color: ${(props) => props.theme.colors.whiteText};
   }
   p:first-of-type {
     font-family: ${(props) => props.theme.fonts.header};
   }
   p:nth-of-type(2) {
     text-align: right;
+  }
+  a{
+    text-align: center;
   }
 `;
